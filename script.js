46f5c44f7f45c9e8dc937db80266f7dc57fac5b3
Layonel Londe <157087322+Layonel-londe@users.noreply.github.com>
2024-05-02 18:36:50 -0300

    changes

--- script.js
+++ script.js
@@ -3,54 +3,49 @@ const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
-const titulo = document.querySelector ('.app__title');
+const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
-const StartPauseBt= document.querySelector('#start-pause')
-
+const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
-const musica =new Audio('/sons/luna-rise-part-one.mp3')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
 let tempoDecorridoEmSegundos = 5
+let intervaloId = null
 
 musica.loop = true
 
-musicaFocoInput.addEventListener ('change', () => {
-    if (musica.paused) {
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
         musica.play()
-    }else {
+    } else {
         musica.pause()
     }
 })
 
-//botões
-
-botaoIniciar.addEventListener('click',() => {
-    html.setAttribute('start-pause', 'app__card-primary-button')
-    
-})
-
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
-    focoBt.classList.add ('active')
+    focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto')
-    titulo.setAttribute('titulo', titulo);
-    curtoBt.classList.add ('active')
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo')
-    longoBt.classList.add ('active')
+    longoBt.classList.add('active')
 })
 
-function alterarContexto (contexto) {
-    botoes.forEach(function( contexto){
+function alterarContexto(contexto) {
+    botoes.forEach(function (contexto){
         contexto.classList.remove('active')
     })
     html.setAttribute('data-contexto', contexto)
-    banner.setAttribute('src',`imagens/${contexto}.png`)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
         case "foco":
             titulo.innerHTML = `
@@ -61,19 +56,41 @@ function alterarContexto (contexto) {
         case "descanso-curto":
             titulo.innerHTML = `
             Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
-            `
+            ` 
             break;
         case "descanso-longo":
             titulo.innerHTML = `
             Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
             `
         default:
-            break;     
+            break;
     }
 }
 
-const  contagemRegressiva =() => {
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0){
+        audioTempoFinalizado.play()
+        alert('Tempo finalizado!')
+        zerar()
+        return
+    }
     tempoDecorridoEmSegundos -= 1
-    
+    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
 }
-StartPauseBt .addEventListener ('click') = contagemRegressiva
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if(intervaloId){
+        audioPausa.play()
+        zerar()
+        return
+    }
+    audioPlay.play()
+    intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar() {
+    clearInterval(intervaloId) 
+    intervaloId = null
+}
